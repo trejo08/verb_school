ea5806e496389f6f01d6d28fce75ccfbd637473f
Juan Trejo <trejocola@gmail.com>
2013-10-21 22:28:00 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VerbSchool::Application.config.secret_token = '6f632dcc1100a72c224984a3eadf6fad978f51bc9021cba723dedd66f1a9e61a08d15b306ecbbb9c7d4966dc7e1d7dc942fd48573c4a09fb8aa7191bebcb7e2d'
