6607455cb05bc742958754583d8415662d3f30c3
Juan Trejo <trejo_cola@hotmail.com>
2013-10-23 15:08:25 -0600

    avances con nested models en examenes

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,1 +1,20 @@
 //= require active_admin/base
+
+
+$(function(){
+	$("form").on("click",".add_fields",function(event){		
+		event.preventDefault();
+		time = new Date().getTime();
+	  regexp = new RegExp($(this).data('id'), 'g');
+	  $(this).before($(this).data('fields').replace(regexp, time));  
+	});
+	       
+
+	   //function for remove fields
+	$("form").on("click", ".remove_fields", function(event){
+		event.preventDefault();
+		var _this = $(this);
+		_this.closest("ol").find("input[type='hidden']").attr("value",1);
+		_this.closest('ol').hide();        	
+	});
+});
