1a0369fc8523530cdd3d2b66e403b8db3b072bb1
Juan Trejo <trejo_cola@hotmail.com>
2013-10-23 16:46:07 -0600

    cambios js activeadmin

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -2,15 +2,16 @@
 
 
 $(function(){
+
+	//function for add fields on nested models
 	$("form").on("click",".add_fields",function(event){		
 		event.preventDefault();
 		time = new Date().getTime();
 		regexp = new RegExp($(this).data('id'), 'g');
 		$(this).before($(this).data('fields').replace(regexp, time));  
 	});
-	       
 
-	   //function for remove fields
+	//function for remove fields
 	$("form").on("click", ".remove_fields", function(event){
 		event.preventDefault();
 		var _this = $(this);
