ac3ed6a9db2e26ff759e3cf85b3156adc9db23e4
Juan Trejo <trejocola@gmail.com>
2013-10-24 10:11:36 -0600

    avances visuales, y borrando el public/index.html

--- app/models/index_page.rb
+++ app/models/index_page.rb
@@ -1,6 +1,6 @@
 class IndexPage < ActiveRecord::Base
 	belongs_to :admin_user
-  attr_accessible :description1, :description2, :description3, :title
+  attr_accessible :description1, :description2, :description3, :title, :admin_user_id
 
   
 end
